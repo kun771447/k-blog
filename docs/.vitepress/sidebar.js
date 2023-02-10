e1820295c913b1981336305200e05450f6902e44
小丶k <kun771447@163.com>
2023-02-10 19:28:09 +0800

    remove: test code

--- docs/.vitepress/sidebar.js
+++ docs/.vitepress/sidebar.js
@@ -1,6 +1,10 @@
 import { createSideBarConfig } from "./util";
 
-const pathArr = ["html", "css", "tcp"];
+const pathArr = [
+"html", 
+// "css", 
+// "tcp"
+];
 
 export const sidebar = pathArr.map((item) =>
   createSideBarConfig(item, "/" + item)
